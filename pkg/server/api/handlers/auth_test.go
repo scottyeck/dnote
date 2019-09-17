7b4dd828f8835f9ed7240767c432bb9ad941e597
Sung Won Cho <sung@monomax.sh>
2019-09-17 19:14:49 +0800

    wip

--- pkg/server/api/handlers/auth_test.go
+++ pkg/server/api/handlers/auth_test.go
@@ -1,18 +1,47 @@
 package handlers
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
-// 	"time"
-//
-// 	"github.com/dnote/dnote/pkg/assert"
-// 	"github.com/dnote/dnote/pkg/clock"
-// 	"github.com/dnote/dnote/pkg/server/api/handlers"
-// 	"github.com/dnote/dnote/pkg/server/database"
-// 	"github.com/dnote/dnote/pkg/server/mailer"
-// 	"github.com/dnote/dnote/pkg/server/testutils"
-// 	"github.com/pkg/errors"
-// 	"gitlab.com/monomax/dnote-infra/test/config"
-// )
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/dnote/dnote/pkg/assert"
+	"github.com/dnote/dnote/pkg/clock"
+	"github.com/dnote/dnote/pkg/server/database"
+	"github.com/dnote/dnote/pkg/server/testutils"
+)
+
+func TestCreateResetToken(t *testing.T) {
+	defer testutils.ClearData()
+	db := database.DBConn
+
+	// Setup
+	server := httptest.NewServer(NewRouter(&App{
+		Clock: clock.NewMock(),
+	}))
+	defer server.Close()
+
+	u := testutils.SetupUserData()
+	testutils.SetupAccountData(u, "alice@example.com", "somepassword")
+
+	dat := `{"email": "alice@example.com"}`
+	req := testutils.MakeReq(server, "POST", "/reset-token", dat)
+
+	// Execute
+	res := testutils.HTTPDo(t, req)
+
+	// Test
+	assert.StatusCodeEquals(t, res, http.StatusOK, "Status code mismtach")
+
+	var tokenCount int
+	testutils.MustExec(t, db.Model(&database.Token{}).Count(&tokenCount), "counting tokens")
+
+	var resetToken database.Token
+	testutils.MustExec(t, db.Where("user_id = ? AND type = ?", u.ID, database.TokenTypeResetPassword).First(&resetToken), "finding reset token")
+
+	assert.Equal(t, tokenCount, 1, "reset_token count mismatch")
+	assert.NotEqual(t, resetToken.Value, nil, "reset_token value mismatch")
+	if resetToken.UsedAt != nil {
+		t.Errorf("used_at should be nil but got: %+v", resetToken.UsedAt)
+	}
+}
