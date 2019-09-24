a58e7d83b3f8920d5913ecacbbcfcfb2aa4a9ad1
Sung Won Cho <sung@monomax.sh>
2019-09-24 18:22:51 +0800

    fix

--- browser/src/scripts/components/App.tsx
+++ browser/src/scripts/components/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import * as React from "react";
 import classnames from "classnames";
 
-import { getShortMonthName } from "../../../../web/src/helpers/time";
+// import * as notesService from 'jslib/services/notes';
 
 interface Props {}
 
 const App: React.FunctionComponent<Props> = () => {
-  return <div>hey, {getShortMonthName(new Date())}</div>;
+  return <div>hey</div>;
 };
 
 export default App;
