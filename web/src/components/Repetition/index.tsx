87faf662a61cb6c265afc2adfffc2955646c1f4a
Sung Won Cho <sung@monomax.sh>
2019-10-05 15:51:40 +0800

    fetch

--- web/src/components/Repetition/index.tsx
+++ web/src/components/Repetition/index.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Helmet from 'react-helmet';
 
+import { getDigestRules } from '../../store/digestRules';
+import { useDispatch } from '../../store';
 import CreateModal from './CreateModal';
 
 const Repetition: React.FunctionComponent = () => {
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDigestRules());
+  }, [dispatch]);
 
   return (
     <div className="page page-mobile-full">
